659f878d55909b5b97a3b4ec2471b1b9cd2fdb0c
RonaldP007 <paniaguarpg007@gmail.com>
2018-09-24 14:34:25 -0600

    terminado

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>tarea</title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <div>
+        <form name="formulario" method="post" action="save.php">
+
+            <br>Nombre: <input type="text" name="nombre" required><br>
+            <br>Apellido: <input type="text" name="apellido" required><br>
+            <br>Correo: <input type="email" name="correo" required><br>
+            <br><input type="submit" value="Guardar" />
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ save.php
@@ -0,0 +1,12 @@
+<?php
+$nombre = $_POST['nombre'];
+//echo $nombre."<br/>";
+$apellido = $_POST['apellido'];
+//echo $apellido."<br/>";
+$correo = $_POST['correo'];
+//echo $correo."<br/>";
+
+echo "<br/> &nbsp; El nombre es: ". $nombre;
+echo "<br/> &nbsp; El apellido es: ". $apellido;
+echo "<br/> &nbsp; El correo es: ". $correo;
+?>
